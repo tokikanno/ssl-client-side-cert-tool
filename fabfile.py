b5aff26d35de004c1a5383f6577b3d60367c0129
toki.kanno <toki.kanno@gmail.com>
2016-02-12 02:17:03 +0800

    fix

--- fabfile.py
+++ fabfile.py
@@ -56,7 +56,7 @@ def gen_client_key(email):
     print blue('generating client key')
 
     local('openssl req -new -newkey rsa:4096 -nodes -keyout %(client_path)s/client.key -out %(client_path)s/client.crt -subj %(subj)s' % ctx)
-    local('chmod 600 %(client_path)s/client.key %(client_path)s/client.crt')
+    local('chmod 600 %(client_path)s/client.key %(client_path)s/client.crt' % ctx)
 
     print blue('signing with ca.key')
     local('openssl x509 -req -days 365 -in %(client_path)s/client.crt -CA ca.crt -CAkey ca.key -CAcreateserial -CAserial ca.serial -out %(client_path)s/sign.crt' % ctx)
